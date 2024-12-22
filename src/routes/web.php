9817779dc2162612683ccb844a03a3b34d4bc025
visualideas <bubnov.alexey@gmail.com>
2024-12-22 15:07:50 +0300

    INIT

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 
 Route::get('/', function () {
+
+    Session::id();
+
+    Cache::remember('test', now()->addDay(), fn()=>true);
+
+    User::all();
+
     return view('welcome');
 });
